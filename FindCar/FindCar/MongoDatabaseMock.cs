a347404f669daee48ac41ba262d13312c80c1e77
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2022-02-26 18:48:00 +0200

    telegram client initialization
        	- add mongodb mock stub class
            - add di for store, botcontroller, botprocessor
    	- throw notimplementedexception() in unimplemented methods

---
+++ FindCar/FindCar/MongoDatabaseMock.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace FindCar
+{
+    public class MongoDatabaseMock: MongoDatabaseBase
+    {
+        public override Task CreateCollectionAsync(string name, CreateCollectionOptions options = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task DropCollectionAsync(string name, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IMongoCollection<TDocument> GetCollection<TDocument>(string name, MongoCollectionSettings settings = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task<IAsyncCursor<BsonDocument>> ListCollectionsAsync(ListCollectionsOptions options = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task RenameCollectionAsync(string oldName, string newName, RenameCollectionOptions options = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task<TResult> RunCommandAsync<TResult>(Command<TResult> command, ReadPreference readPreference = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IMongoClient Client { get; }
+        public override DatabaseNamespace DatabaseNamespace { get; }
+        public override MongoDatabaseSettings Settings { get; }
+    }
+}
