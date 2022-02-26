a30fb4d0bfa8364dda8849cfd24f1f7084579924
Dmytro Bashynskiy <sbmbash@gmail.com>
2022-02-26 14:10:44 +0200

    ++

--- FindCar/FindCar/Bot/Store.cs
+++ FindCar/FindCar/Bot/Store.cs
@@ -1,15 +1,36 @@
 using System;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace FindCar.Bot
 {
+    public class JsonWrap
+    {
+        public string TypeName { get; set; }
+        public string Payload { get; set; }
+    }
+    
     public record Passenger(string ChatId, string From, string To, string SeatCount, string Message, string Phone, string Telegram);
     
     public class Store
     {
+        
+        // предлагаю реализовать стор через сериализацию с поддекржкой полиморфизма или заюзать 
+        // сериализацию монги, она тоже умеет в сериализацию классов с наследованием
+        // важно чтоб при десериализации по IBotState мы получали инстанс конкретного стейта
+        
+        // вариант руками все заворачивать в JsonWrap
+        // и по TypeName десериализировать в конкретный инстанс
+        private readonly IMongoDatabase _db;
+
+        public Store(IMongoDatabase db)
+        {
+            _db = db;
+        }
+        
         public Task SaveState<T>(long chatId, T state) where T : IBotState
         {
-            
+            //var state = _db.GetCollection<JsonWrap>();
         }
 
         public Task<IBotState> GetState(long chatId)
